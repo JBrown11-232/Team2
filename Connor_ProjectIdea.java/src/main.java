1091b06c748b811d7640a8055f75949c0d6bb3dd
JBrown11-232 <104179428+JBrown11-232@users.noreply.github.com>
2022-04-26 22:06:07 -0400

    Delete main.java

--- Connor_ProjectIdea.java/src/main.java
+++
@@ -1,7 +0,0 @@
-public class main {
-    public static void main(String[] args){
-        System.out.println("A financial calculator that keeps track and provides information of your current" +
-                " income and expenses for different aspects of life (gas, bills, food...etc.)");
-    }
-
-}
