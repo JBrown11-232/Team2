673411f1778d4e495d49654b0cfe611a7dd19180
Connor <connor1031levinson@gmail.com>
2022-04-26 20:40:52 -0400

    Made a java class that prints my idea for the project.

---
+++ Connor_ProjectIdea.java/src/main.java
@@ -0,0 +1,7 @@
+public class main {
+    public static void main(String[] args){
+        System.out.println("A financial calculator that keeps track and provides information of your current" +
+                " income and expenses for different aspects of life (gas, bills, food...etc.)");
+    }
+
+}
